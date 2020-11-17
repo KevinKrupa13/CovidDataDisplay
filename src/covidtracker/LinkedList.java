7104e94068c57cc01feafa1c4add568f96f046c6
Kevin Krupa <kevinkrupa13@vt.edu>
2020-11-16 22:39:11 -0500

    Linked List Update

--- src/covidtracker/LinkedList.java
+++ src/covidtracker/LinkedList.java
@@ -3,5 +3,27 @@ package covidtracker;
 import list.ListInterface;
 
 public class LinkedList<T> implements ListInterface<T> {
-
+    
+    private int size;
+    private Node<T> head;
+    
+    private static class Node<T> {
+        
+        private T data;
+        private Node<T> next;
+        
+        public Node(T data) {
+            this(data, null);
+        }
+        
+        public Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+        
+        
+        
+    }
+    
+    
 }
