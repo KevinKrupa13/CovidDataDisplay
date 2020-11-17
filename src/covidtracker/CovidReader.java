e7b8403457de56da1c99684a86d2b3a06b57444a
Kevin Krupa <kevinkrupa13@vt.edu>
2020-11-17 11:07:43 -0500

    Starting work on covidreader class

--- src/covidtracker/CovidReader.java
+++ src/covidtracker/CovidReader.java
@@ -1,5 +1,30 @@
 package covidtracker;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CovidReader {
+
+    private LinkedList<Race> races;
+    private State[] states;
+
+    public CovidReader(String fileName) throws FileNotFoundException {
+        races = raceReader(fileName);
+        states = stateReader(fileName);
+    }
+
+
+    private LinkedList<Race> raceReader(String fileName)
+        throws FileNotFoundException {
+        LinkedList<Race> temp = new LinkedList<Race>();
+        Scanner file = new Scanner(new File(fileName));
+    }
+
+
+    private State[] stateReader(String fileName) throws FileNotFoundException {
+        State[] temp = new State[6];
+        Scanner file = new Scanner(new File(fileName));
+    }
 
 }
