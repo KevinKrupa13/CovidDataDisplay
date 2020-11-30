a14de2f3f51efa9e1548680f1d13181c76ace1d0
suhad <suhad@vt.edu>
2020-11-30 19:23:04 +0000

    changed GUI name

---
+++ src/prj5/GUIWindow.java
@@ -0,0 +1,43 @@
+package prj5;
+
+import cs2.Button;
+import cs2.Window;
+
+public class GUIWindow {
+
+    private Window window;
+    private Button repDC;
+    private Button repGA;
+    private Button repMD;
+    private Button repNC;
+    private Button repTN;
+    private Button repVA;
+    private Button quit;
+    private Button SortByAlpha;
+    private Button SortByCFR;
+
+    public GUIWindow(Input filereader) {
+        window = new Window("Covid data visualisation");
+        quit = new Button("Quit");
+        quit.onClick(this, "clickedQuit");
+        SortByAlpha = new Button("Sort By Alpha");
+        SortByCFR = new Button("Sort By CFR");
+        repDC = new Button("Represent DC");
+        repGA = new Button("Represent GA");
+        repMD = new Button("Represent MD");
+        repNC = new Button("Represent NC");
+        repTN = new Button("Represent TN");
+        repVA = new Button("Represent VA");
+    }
+
+
+    /**
+     * clickedQuit method
+     * 
+     * @param button
+     */
+    public void clickedQuit(Button button) {
+        System.exit(0);
+    }
+
+}
