0b21295b5545d16d7acc31c1dd9369f405aa526a
suhad <suhad@vt.edu>
2020-11-30 19:03:11 +0000

    adding GUI class

---
+++ src/prj5/CovidGUI.java
@@ -0,0 +1,20 @@
+package prj5;
+
+import cs2.Button;
+import cs2.Window;
+
+public class CovidGUI {
+    
+    private Window window;
+    private Button repDC;
+    private Button repGA;
+    private Button repMD;
+    private Button repNC;
+    private Button repTN;
+    private Button repVA;
+    private Button quit;
+    private Button byAlpha;
+    private Button byCFR;
+    
+
+}
